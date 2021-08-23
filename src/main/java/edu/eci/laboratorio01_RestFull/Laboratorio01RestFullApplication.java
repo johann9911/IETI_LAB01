3f02febf0380a107a3dbfc3a9d426bccb24182bf
Johann Bogota <johann.bogota@escuelaing.edu.co>
2021-08-23 15:00:43 -0500

    Creacion template spring boot

---
+++ src/main/java/edu/eci/laboratorio01_RestFull/Laboratorio01RestFullApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.laboratorio01_RestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio01RestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio01RestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/laboratorio01_RestFull/Laboratorio01RestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.laboratorio01_RestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio01RestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
